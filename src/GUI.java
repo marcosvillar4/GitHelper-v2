f1d95c41720a741df0c07d7f3171bbc85a648120
marcos-villar <marcos.villar.mail@gmail.com>
2024-06-16 18:34:19 -0300

    + Interfaz basica + Creacion de repo

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,39 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class GUI {
-    private JLabel test;
     private JPanel MainPage;
+    private JTextField nombreRepo;
+    private JButton confirm;
+    private JCheckBox priv;
+
+    public GUI() {
+        confirm.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                String repoName = nombreRepo.getText();
+                boolean repoPriv = priv.isSelected();
+                String repoPrivacy = "";
+
+                if (repoPriv){
+                    repoPrivacy = "--private";
+                }
+                else {
+                    repoPrivacy = "--public";
+                }
+
+                Runtime shell = Runtime.getRuntime();
+
+
+                try {
+                    shell.exec(("gh repo create " + repoName  + " " + repoPrivacy));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("GitHelper V2");
@@ -10,5 +41,8 @@ public class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+
+
     }
 }
